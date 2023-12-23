bd362f5ace919aea1076f4631834db389646f1bd
Jason <sysbender@gmail.com>
2023-12-22 20:13:44 -0500

    conditional set class

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 // import ReactDOM from "react-dom";
 function Pizza({ pizzaObj }) {
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
