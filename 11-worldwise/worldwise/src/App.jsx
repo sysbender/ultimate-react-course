75dab6d6063e1cbc3b2649536258fbc56ba19059
Jason <sysbender@gmail.com>
2023-12-30 23:00:27 -0500

    use css module

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -4,14 +4,15 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import PageNav from "./components/PageNav";
+import AppLayout from "./pages/AppLayout";
 function App() {
   return (
-    <PageNav/>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- 11-worldwise/worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/worldwise/src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
 function Homepage() {
   return (
     <div>
       <PageNav />
       <h2>Worldwise</h2>
+      <Link to="/app">Go to app</Link>
     </div>
   );
 }
