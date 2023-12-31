06e98179f945b86b9f7015ac31d1d180be1b6b0e
Jason <sysbender@gmail.com>
2023-12-31 12:38:33 -0500

    add PageNav to all pages

--- 11-worldwise/starter/components/PageNav.jsx
+++ 11-worldwise/starter/components/PageNav.jsx
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
-import Logo from "./Logo";
-
 function PageNav() {
   return (
     <nav className={styles.nav}>
-      <Logo />
       <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
         <li>
           <NavLink to="/product">Product</NavLink>
         </li>
@@ -14,9 +14,7 @@ function PageNav() {
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
-          <NavLink to="/login" className={styles.ctaLink}>
-            Login
-          </NavLink>
+          <NavLink to="/login">Login</NavLink>
         </li>
       </ul>
     </nav>
